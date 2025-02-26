558d0f97b4e7c6a7a051776058047c55b23be8b0
Muhammad Anas <anassoomro23@gmail.com>
2025-02-26 22:41:03 +0500

    need to align the logo

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,41 +1,62 @@
 import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import { TbLockPassword } from "react-icons/tb";
+import Logo from "../assets/pictures/logo/Group 802.jpg";
 
 export default function Singin() {
   return (
-    <div className="pt-[211px] pb-[133px] pr-[293px] pl-[293px] flex justify-center  ">
-      <div className="absolute border-2 border-gray-300 bg-white p-4 shadow-2xl rounded-lg">
-        <h2
-          className="text-center mb-4 pt-12 tracking-wider text-lg font-normal">
-          Please enter your login details
-        </h2>
-        <div
-          className="pl-[101px] pr-[102px]">
-          <div className="relative flex items-center w-full mt-10">
-            <AiOutlineMail className="absolute ml-3 mt-3.5  -translate-y-1/2 text-gray-500 text-lg" />
-            <input 
-              type="email"
-              placeholder="Email"
-              className="w-[363px] p-2 pl-10 border rounded border-l-4 border-yellow-500 outline-none"
-            />
-          </div>
+    <>
+      <div className=" min-h-screen flex-none  py-20 px-6 md:px-20">
+        {/* logo section/ */}
+        <div className="mb-6   ">
+          <img src={Logo} className="w-32  md:w-40" />
+        </div>
 
-          <div className="relative flex items-center w-full mt-4">
-            <TbLockPassword className="absolute ml-3 -translate-y-1/2 text-gray-500 text-lg" />
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-[363px] p-2 mb-3 pl-10 border rounded border-l-4 border-yellow-500 outline-none"
-            />
+        {/* //form section  */}
+        <div className="flex flex-col items-center">
+          <div className="border-2  border-gray-300 bg-white p-6 shadow-2xl rounded-lg w-full max-w-md">
+            <h2 className="text-center mb-6 tracking-wider text-lg font-normal">
+              Please enter your login details
+            </h2>
+
+            <div className="w-full">
+              {/* Email Input */}
+              <div className="relative flex items-center w-full mt-4">
+                <AiOutlineMail className="absolute left-3 text-gray-500 text-lg" />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="w-full p-3 pl-10 rounded border-l-4 border-yellow-500 outline-none"
+                />
+              </div>
+
+              {/* Password Input */}
+              <div className="relative flex items-center w-full mt-4">
+                <TbLockPassword className="absolute left-3 text-gray-500 text-lg" />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-full p-3 pl-10 rounded border-l-4 border-yellow-500 outline-none"
+                />
+              </div>
+
+              {/* Sign-in Button */}
+              <button className="w-full bg-yellow-500 hover:bg-yellow-400 text-white p-3 mt-4 rounded transition duration-300">
+                Sign in
+              </button>
+
+              {/* Forgot Password */}
+
+              <a
+                href="#"
+                className="block text-center text-blue-500 mt-4 hover:underline"
+              >
+                Forgotten Password?
+              </a>
+            </div>
           </div>
-          <button className="w-full hover:bg-amber-300 theme-color text-white p-2 mt-2.5 rounded">
-            Sign in
-          </button>
-          <a href="" className="justify-center hover:underline flex text-blue-500 mt-5">Forgotten Password?</a>
         </div>
       </div>
-      //{" "}
-    </div>
+    </>
   );
 }
